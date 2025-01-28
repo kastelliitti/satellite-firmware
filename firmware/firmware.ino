5367ee829ec1d8fdbfd53026c22f86f26042f91c
alticator <main@alticator.com>
2025-01-28 22:11:23 +0200

    Essential Satellite Functions

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,18 +1,81 @@
 #include "CanSatNeXT.h"
+#include <Wire.h>
+#include "ADS1X15.h"
+
+ADS1115 ADS(0x48);
+
+int MODE = 0;
 
 void setup() {
   Serial.begin(115200);
   CanSatInit(42);
-  Serial.println("TRANSMISSION START");
-
+  if (!ADS.begin()) {
+    Serial.println("ADS1115 not found on I2C bus!");
+  } else {
+    Serial.println("ADS1115 initialized successfully.");
+  }
+  ADS.setGain(0);
+  Serial.println("READY");
 }
 
 void loop() {
+  if (MODE == 0) {
+    prelaunchMode();
+  } else if (MODE == 1) {
+    missionMode();
+  } else if (MODE == 2) {
+    postmissionMode();
+  } else {
+    standbyMode();
+  }
+}
+
+void prelaunchMode() {
+  Serial.println("Waiting for launch...");
+  uint8_t package = 42;
+  sendData(&package, 1);
+  delay(200);
+}
+
+void standbyMode() {
+  uint8_t package = 28;
+  sendData(&package, 1);
+  delay(200);
+}
+
+void missionMode() {
   float temperature = readTemperature();
   float pressure = readPressure();
+  int16_t adcValue = ADS.readADC(0);
   float ldrVoltage = analogRead(LDR);
-  float ax, ay, az;
-  readAcceleration(ax, ay, az);
-  sendData((String)temperature + "," + (String)pressure + "," + (String)ldrVoltage + "," + (String)ax + "," + (String)ay + "," + (String)az);
-  delay(500);
+  float voltageMeter = -100.0;
+  if (adcValue != -101) {
+    voltageMeter = ADS.toVoltage(adcValue);
+  }
+  float acceleration[3];
+  readAcceleration(acceleration[0], acceleration[1], acceleration[2]);
+
+  uint8_t package[28];
+  memcpy(&package[0], &temperature, 4);
+  memcpy(&package[4], &pressure, 4);
+  memcpy(&package[8], &ldrVoltage, 4);
+  memcpy(&package[12], &voltageMeter, 4);
+  for (int i = 0; i < 3; i++) {
+    memcpy(&package[16 + (4 * i)], &acceleration[i], 4);
+  }
+  sendData(&package, 28);
+  delay(200);
+}
+
+void postmissionMode() {
+  standbyMode();
+}
+
+void onBinaryDataReceived(const uint8_t *data, uint16_t len){
+  int newMode = 0;
+  memcpy(&newMode, data, len);
+  Serial.println((String)newMode);
+  if (newMode >= 0 && newMode < 3) {
+    MODE = newMode;
+  }
 }

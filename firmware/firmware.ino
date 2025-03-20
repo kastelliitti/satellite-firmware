b8322d6105954369483c14c8f10c7aa7e53561bf
alticator <main@alticator.com>
2025-03-20 19:01:35 +0200

    Add new fields and change temperature reading to NTC
    1. Add gyroscope and battery voltage readings
    2. Change temperature sensor to NTC thermistor (conversion to Celsius not working properly yet)

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,6 +4,11 @@
 
 ADS1115 ADS(0x48);
 
+// GET FROM THERMISTOR DATASHEET
+int THERMISTOR_B = 3950;
+int THERMISTOR_T0 = 298.15;
+int THERMISTOR_R0 = 10000;
+
 int MODE = 0;
 int INTERVAL = 200;
 
@@ -45,7 +50,11 @@ void standbyMode() {
 }
 
 void missionMode() {
-  float temperature = readTemperature();
+  float thermistorV = analogReadVoltage(NTC);
+  float rTherm = THERMISTOR_R0*(thermistorV/(3.3-thermistorV));
+  float temperatureK = THERMISTOR_B/(log(rTherm/THERMISTOR_R0)+(THERMISTOR_B/THERMISTOR_T0));
+  float temperature = temperatureK - 273.15;
+
   float pressure = readPressure();
   int16_t adcValue = ADS.readADC(0);
   float ldrVoltage = analogRead(LDR);
@@ -56,15 +65,24 @@ void missionMode() {
   float acceleration[3];
   readAcceleration(acceleration[0], acceleration[1], acceleration[2]);
 
-  uint8_t package[28];
-  memcpy(&package[0], &temperature, 4);
-  memcpy(&package[4], &pressure, 4);
-  memcpy(&package[8], &ldrVoltage, 4);
-  memcpy(&package[12], &voltageMeter, 4);
+  float gyro[3];
+  readGyro(gyro[0], gyro[1], gyro[2]);
+
+  float batteryVoltage = analogReadVoltage(BATT);
+
+  uint8_t package[44];
+  memcpy(&package[0], &temperature, 4);    // 0-3
+  memcpy(&package[4], &pressure, 4);       // 4-7
+  memcpy(&package[8], &ldrVoltage, 4);     // 8-11
+  memcpy(&package[12], &voltageMeter, 4);  // 12-15
   for (int i = 0; i < 3; i++) {
-    memcpy(&package[16 + (4 * i)], &acceleration[i], 4);
+    memcpy(&package[16 + (4 * i)], &acceleration[i], 4);  // 16-19, 20-23, 24-27
   }
-  sendData(&package, 28);
+  for (int i = 0; i < 3; i++) {
+    memcpy(&package[28 + (4 * i)], &gyro[i], 4);  // 28-31, 32-35, 36-39
+  }
+  memcpy(&package[40], &batteryVoltage, 4);  // 40-43
+  sendData(&package, 44);
   delay(INTERVAL);
 }
 

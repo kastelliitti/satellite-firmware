e3a71c19435575228b9dafdafab017c5b9d88ba6
alticator <main@alticator.com>
2025-03-25 21:36:15 +0200

    Add thermistor readings and SD card saving
    Also change to channel 7

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,20 +5,40 @@
 ADS1115 ADS(0x48);
 
 // GET FROM THERMISTOR DATASHEET
-int THERMISTOR_B = 3950;
-int THERMISTOR_T0 = 298.15;
-int THERMISTOR_R0 = 10000;
+const float THERMISTOR_B = 3977.0f;
+const float THERMISTOR_T0 = 298.15f;
+const float THERMISTOR_R0 = 10000.0f;
+
+// SD CARD SETTINGS
+bool SD_PRESENT = false;
+String FILENAME = "/savefile.csv";
 
 int MODE = 0;
 int INTERVAL = 200;
 
 void setup() {
   Serial.begin(115200);
+  int saveFileId = 0;
+  bool saveFileIdFound = false;
+  while (!saveFileIdFound) {
+    String fileNameCandidate = "/savefile" + saveFileId + ".csv";
+    if (!fileExists(fileNameCandidate)) {
+      FILENAME = fileNameCandidate;
+      saveFileIdFound = true;
+    }
+    saveFileId++;
+  }
+  setRadioChannel(7);
   CanSatInit(42);
   if (!ADS.begin()) {
+
     Serial.println("ADS1115 not found on I2C bus!");
   } else {
     Serial.println("ADS1115 initialized successfully.");
+  }
+  SD_PRESENT = SDCardPresent();
+  if (SD_PRESENT) {
+    appendFile(FILENAME, "timestamp, signal_strength, temp(C), r_thermistor(ohm), pressure(mbar), ldr(V), voltage(V), ax(G), ay(G), az(G), gx(mrad/s), gy(mrad/s), gz(mrad/s), battery_voltage(V)\n");
   }
   ADS.setGain(0);
   Serial.println("READY");
@@ -51,9 +71,10 @@ void standbyMode() {
 
 void missionMode() {
   float thermistorV = analogReadVoltage(NTC);
-  float rTherm = THERMISTOR_R0*(thermistorV/(3.3-thermistorV));
-  float temperatureK = THERMISTOR_B/(log(rTherm/THERMISTOR_R0)+(THERMISTOR_B/THERMISTOR_T0));
+  float rTherm = THERMISTOR_R0 * ( 1 / ( (3.3 / thermistorV) - 1) );
+  float temperatureK = THERMISTOR_B / ( log(rTherm / THERMISTOR_R0) + (THERMISTOR_B / THERMISTOR_T0) );
   float temperature = temperatureK - 273.15;
+  //float temperature = rTherm;
 
   float pressure = readPressure();
   int16_t adcValue = ADS.readADC(0);
@@ -83,6 +104,11 @@ void missionMode() {
   }
   memcpy(&package[40], &batteryVoltage, 4);  // 40-43
   sendData(&package, 44);
+
+  appendFile(FILENAME, String(millis()) + "," + String(temperature, 16) + "," + String(rTherm, 16) + "," + String(pressure, 16) + ","
+    + String(ldrVoltage, 16) + "," + String(voltageMeter, 16) + "," + String(acceleration[0], 16) + ","
+    + String(acceleration[1], 16) + "," + String(acceleration[2], 16) + "," + String(gyro[0], 16) + ","
+    + String(gyro[1], 16) + "," + String(gyro[2], 16) + "," + String(batteryVoltage, 16) + "\n");
   delay(INTERVAL);
 }
 
